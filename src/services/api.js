93ae26ed2ff239576886995b27d5ea11b0959e80
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-11-30 22:42:41 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,14 @@
 import axios from "axios";
 
-// export const api = axios.create({
-//   baseURL: "https://project-winevo-backend.onrender.com/api",
-//   withCredentials: true,
-// });
-// export const api = axios.create({
-//   baseURL: "https://www.api.winevo-collection.me/api",
-//   withCredentials: true,
-// });
-
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://www.api.winevo-collection.me/api",
   withCredentials: true,
 });
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:8080/api",
+//   withCredentials: true,
+// });
 
 export const setAuthHeader = (token) => {
   api.defaults.headers.common.Authorization = `Bearer ${token}`;
