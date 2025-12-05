ee28eed87fb825e18b472368b74f90cd1f247ae4
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-12-05 06:30:45 +0100

    Update RequireAdmin.jsx

--- src/Layouts/RequireAdmin.jsx
+++ src/Layouts/RequireAdmin.jsx
@@ -7,20 +7,20 @@ import {
 } from "../redux/auth/selectors";
 
 export default function RequireAdmin() {
-  // const isLoggedIn = useSelector(selectIsLoggedIn);
-  // const isRefreshing = useSelector(selectIsRefreshing);
-  // const role = useSelector(selectRole);
-  // const location = useLocation();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const role = useSelector(selectRole);
+  const location = useLocation();
 
-  // if (isRefreshing) return <div>Loading...</div>;
+  if (isRefreshing) return <div>Loading...</div>;
 
-  // if (!isLoggedIn) {
-  //   return <Navigate to="/admin/signin" replace state={{ from: location }} />;
-  // }
+  if (!isLoggedIn) {
+    return <Navigate to="/admin/signin" replace state={{ from: location }} />;
+  }
 
-  // if (role !== "admin") {
-  //   return <Navigate to="/home" replace />;
-  // }
+  if (role !== "admin") {
+    return <Navigate to="/home" replace />;
+  }
 
   return <Outlet />;
 }
